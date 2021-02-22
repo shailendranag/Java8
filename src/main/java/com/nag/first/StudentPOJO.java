f4eb6ee45c2f6738d7154f15695f4f3b7ce097d1
nag <sailunag18@gmail.com>
2021-02-22 10:26:22 +0530

    First

--- src/main/java/com/nag/first/StudentPOJO.java
+++ src/main/java/com/nag/first/StudentPOJO.java
@@ -1,23 +1,17 @@
 package com.nag.first;
 
-public class StudentPOJO implements Comparable <StudentPOJO> {
+import java.util.Comparator;
+
+public class StudentPOJO {
 
     int rollno;
     String Name;
     int age;
 
-    StudentPOJO(int rollno,String name,int age){
-        this.rollno=rollno;
-        this.Name=name;
-        this.age=age;
+    StudentPOJO(int rollno, String name, int age) {
+        this.rollno = rollno;
+        this.Name = name;
+        this.age = age;
     }
 
-    public int compareTo(StudentPOJO st) {
-        if(age== st.age)
-            return 0;
-        else if(age>st.age)
-            return 1;
-        else
-            return -1;
-    }
 }
